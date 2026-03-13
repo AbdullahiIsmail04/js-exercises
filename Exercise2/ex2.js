1d45f884ab9f7379e7f51e142c3b53ed57aaf24c
AbdullahiIsmail04 <abdullahiismailali04@gmail.com>
2026-03-13 14:55:30 +0300

    Updated Exercise 1 and Solved Exercise 2

---
+++ Exercise2/ex2.js
@@ -0,0 +1,27 @@
+// Comparison Operators
+let a = 15;
+let b = "15";
+
+// -> Equal to operator
+console.log(a == b);
+
+// -> Strictly Equal to operator
+console.log(a === b);
+
+// -> Not Equal to operator
+console.log(a != b);
+
+// -> Strictly Not Equal to operator
+console.log(a !== b);
+
+// -> Greater than operator
+console.log(a > b);
+
+// -> Less than operator
+console.log(a < b);
+
+// -> Greater than or equal to operator
+console.log(a >= b);
+
+// -> Less than or equal to operator
+console.log(a <= b);
