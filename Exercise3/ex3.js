941d0e26884ef4f87f2201592fe302e4234b4d8f
AbdullahiIsmail04 <abdullahiismailali04@gmail.com>
2026-03-13 15:19:07 +0300

    Solved Exercise 3

---
+++ Exercise3/ex3.js
@@ -0,0 +1,18 @@
+// Logical Operators
+let isSunny = true;
+let isWeekend = true;
+
+// Using logical AND (&&)
+console.log("Using logical AND (&&):");
+console.log(isSunny && isWeekend);
+console.log("You can go to the beach!");
+
+// Using logical OR (||)
+console.log("\nUsing logical OR (||):");
+console.log(isSunny || isWeekend);
+console.log("You can go to the beach!");
+
+// Using logical NOT (!)
+console.log("\nUsing logical NOT (!):");
+console.log(!isSunny);
+console.log("You cannot go to the beach!");
