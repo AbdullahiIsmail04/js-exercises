4048dccf35cf7d51ef0827a55f4dca64c0f08459
AbdullahiIsmail04 <abdullahiismailali04@gmail.com>
2026-03-14 19:20:08 +0300

    Solved JS Exercise 4

---
+++ Exercise4/ex4.js
@@ -0,0 +1,17 @@
+// Regular function declaration
+function add(a , b) {
+    return a + b;
+}
+
+console.log(add(10,10));
+console.log(add(20,10));
+console.log(add(30,10));
+
+// Function expression
+const sum = function(x, y) {
+    return x + y;
+}
+
+console.log(sum(10,50));
+console.log(sum(20,60));
+console.log(sum(30,70));
