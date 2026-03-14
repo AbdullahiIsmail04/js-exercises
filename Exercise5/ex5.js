4620da4129c55afecdfaffa0047a8507fe0e04b4
AbdullahiIsmail04 <abdullahiismailali04@gmail.com>
2026-03-14 22:04:16 +0300

    Solved JS Exercise 5

---
+++ Exercise5/ex5.js
@@ -0,0 +1,9 @@
+// Arrow function to add two numbers
+const add = (a , b) => {
+    return a + b;
+}
+
+console.log(add(5,5));
+console.log(add(10,5));
+console.log(add(20,5));
+console.log(add(30,5));
